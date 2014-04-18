8b15ac1a3c4e2b202727c7ad1bb7338c48955223
Félix Saparelli <felix@passcod.name>
2014-04-18 22:49:44 +1200

    Use domready from jsDelivr

--- static/script.js
+++ static/script.js
@@ -1,8 +1,3 @@
-/*! <-- Until jsDelivr updates it
- * domready (c) Dustin Diaz 2014 - License MIT
- */
-!function(e,t){typeof module!="undefined"?module.exports=t():typeof define=="function"&&typeof define.amd=="object"?define(t):this[e]=t()}("domready",function(){var e=[],t,n=document,r="DOMContentLoaded",i=/^loaded|^i|^c/.test(n.readyState);return i||n.addEventListener(r,t=function(){n.removeEventListener(r,t),i=1;while(t=e.shift())t()}),function(t){i?t():e.push(t)}})
-
 (function() {
   var originals = [];
   var flip, flipper = function(night) {
